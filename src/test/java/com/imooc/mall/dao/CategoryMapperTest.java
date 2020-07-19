80096355b1e5a70e3f8bca99a31df6085a6c9231
zhoufeng <17671461759@163.com>
2020-07-19 18:09:16 +0800

    类目测试

---
+++ src/test/java/com/imooc/mall/dao/CategoryMapperTest.java
@@ -0,0 +1,19 @@
+package com.imooc.mall.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CategoryMapperTest {
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void queryById() {
+    }
+}
